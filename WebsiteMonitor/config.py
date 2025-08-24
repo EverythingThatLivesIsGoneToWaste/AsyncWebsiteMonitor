43bd638643b84e9e0be28d545dff2606ca8a057b
UnknownFruit <kurianovip@gmail.com>
2025-08-24 20:13:40 +0400

    fixing website list

--- WebsiteMonitor/config.py
+++ WebsiteMonitor/config.py
@@ -2,9 +2,8 @@ WEBSITES = [
     "https://google.com",
     "https://github.com",
     "https://stackoverflow.com",
-    "https://shtab.navalny.com",
     "https://planner5d.com",
-    "https://chat.deepseek.com"
+    "https://deepseek.com"
 ]
 
 CHECK_INTERVAL = 10

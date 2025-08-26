eae25c08056b230297e61fb7fe6f6286e8d605b5
UnknownFruit <kurianovip@gmail.com>
2025-08-26 19:44:35 +0400

    testing

--- WebsiteMonitor/main.py
+++ WebsiteMonitor/main.py
@@ -12,7 +12,6 @@ async def main():
         while True:
             await monitor.check_all_websites()
             monitor.print_results()
-
             await asyncio.sleep(CHECK_INTERVAL)
 
     except (Exception, asyncio.CancelledError) as e:
